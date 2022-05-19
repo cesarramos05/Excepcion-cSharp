3c7880b4227c37acd9c5487c69cbc48169776d10
cesarramos05 <43106466+cesarramos05@users.noreply.github.com>
2022-05-19 12:12:14 -0700

    C#

---
+++ Excepcion/Excepcion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepcion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+            catch (FormatException e)
+            {
+                Console.Write(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
